5a53710740d2ae6e7447980be485104ba1d6db59
Andre Miras <andre.miras@gmail.com>
2021-07-23 21:41:00 +0200

    :lipstick: Setup React Bootstrap & FontAwesome fixes #15, fixes #22
    - :lipstick: basic layout setup
    - :heavy_plus_sign: bootstrap react-bootstrap
    - :heavy_plus_sign: @fortawesome/fontawesome-free
    - :heavy_plus_sign: @fortawesome/react-fontawesome
    - :heavy_plus_sign: @fortawesome/free-solid-svg-icons
    - :heavy_plus_sign: @fortawesome/fontawesome-svg-core
    - :heavy_plus_sign: @fortawesome/free-brands-svg-icons
    - :heavy_plus_sign: @fortawesome/free-regular-svg-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import Header from './components/Header'
+
+library.add(fas, fab, far)
 
 function App (): JSX.Element {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Container>
+        MyNotif
+      </Container>
     </div>
   )
 }
