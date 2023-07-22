1c1183db8ad03a18cd59322e026be82e8f6d46b8
Issa <issa.diallo@lemonlearning.fr>
2023-07-22 16:08:21 +0200

    :wrench: remove title all app

--- src/pages/patients/PatientCreatePage.tsx
+++ src/pages/patients/PatientCreatePage.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react'
 import { Patient, defaultPatient } from '../../types'
 import PatientForm from '../../components/forms/PatientForm'
-import useTranslationHook from '../../hook/TranslationHook'
-import TitlePage from '../../components/TitlePage'
 
 const PatientCreatePage = (): JSX.Element => {
   const [newPatient] = useState<Patient>(defaultPatient)
-  const { t } = useTranslationHook()
 
   return (
     <div>
-      <TitlePage title={t('title.addPatient')} />
       <PatientForm patient={newPatient} isEditForm={false} />
     </div>
   )
