eff7b63c5518c3957bb61dc3c061d988d4ddb1ee
issa-diallo <issa.djehon@gmail.com>
2024-04-01 21:38:06 +0200

    :bug: Fix external links for admin redirection
    Introduces the ability to handle external URLs in the FooterNav
    A new prop `isExternal` has been added
    to differentiate between internal and external links

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -7,19 +7,31 @@ interface FooterNavProps {
   url: string
   icon: IconName
   title: string
+  isExternal?: boolean
 }
 
-const FooterNav = ({ url, icon, title }: FooterNavProps): JSX.Element => {
+const FooterNav = ({ url, icon, title, isExternal = false }: FooterNavProps): JSX.Element => {
   const location = useLocation()
   const highlightOnPathname = (pathname: string): string => location.pathname === pathname ? 'text-primary' : ''
+
+  const linkContent = (
+    <div className={`d-flex flex-column align-items-center ${highlightOnPathname(url)}`}>
+      <FontAwesomeIcon icon={['fas', icon]} className='fa-lg' />
+      <div className='mt-1'>{title}</div>
+    </div>
+  )
+
   return (
     <Nav className='me-auto'>
-      <Nav.Link as={Link} to={url}>
-        <div className={`d-flex flex-column align-items-center ${highlightOnPathname(url)}`}>
-          <FontAwesomeIcon icon={['fas', icon]} className='fa-lg' />
-          <div className='mt-1'>{title}</div>
-        </div>
-      </Nav.Link>
+      {isExternal ? (
+        <Nav.Link as='a' href={url} target='_blank' rel='noopener noreferrer'>
+          {linkContent}
+        </Nav.Link>
+      ) : (
+        <Nav.Link as={Link} to={url}>
+          {linkContent}
+        </Nav.Link>
+      )}
     </Nav>
   )
 }
