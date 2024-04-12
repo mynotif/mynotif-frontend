4f219876bb1810f3e74eba82ecb7235a9a725a49
issa-diallo <issa.djehon@gmail.com>
2024-04-12 17:23:35 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
@@ -28,66 +29,119 @@ import ResetPassword from './pages/authentifications/ResetPassword'
 import NewResetPassword from './pages/authentifications/NewResetPassword'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
-import OneSignalManager from './services/oneSignal/OneSignalManager'
+// import OneSignalManager from './services/oneSignal/OneSignalManager'
 
 library.add(fas, fab, far)
 
+declare global {
+  interface Window { OneSignal: any }
+}
+
+window.OneSignal = window.OneSignal || []
+const OneSignal = window.OneSignal
+
 function App (): JSX.Element {
+  useEffect(() => {
+    OneSignal.push(() => {
+      OneSignal.init(
+        {
+          appId: '2849d196-e65f-4779-89ae-4481702276e4',
+          promptOptions: {
+            slidedown: {
+              enabled: true,
+              actionMessage: "We'd like to show you notifications for the latest news and updates about the following categories.",
+              acceptButtonText: 'OMG YEEEEESS!',
+              cancelButtonText: 'NAHHH',
+              categories: {
+                tags: [
+                  {
+                    tag: 'medecin',
+                    label: 'Docteur'
+                  },
+                  {
+                    tag: 'kine',
+                    label: 'Kine'
+                  },
+                  {
+                    tag: 'nurse',
+                    label: 'Infirmiere'
+                  },
+                  {
+                    tag: 'ostheo',
+                    label: 'ostheo'
+                  }
+                ]
+              }
+            }
+          },
+          welcomeNotification: {
+            title: 'OrdoPro',
+            message: 'Thanks for subscribing!'
+          }
+        },
+        // Automatically subscribe to the new_app_version tag
+        OneSignal.sendTag('ordopro', 'nurse', (tagsSent: any) => {
+          // Callback called when tag has finished sending
+          console.log('nurse', tagsSent)
+        })
+      )
+    })
+  }, [])
   return (
     <div className='App bg-light'>
       <BrowserRouter>
-        <OneSignalManager>
-          <TokenContextProvider>
-            <ProfileContextProvider>
-              <FlashMessageContextProvider>
-                <Header />
-                <Container className='mb-5 pb-5'>
-                  <FlashMessages />
-                  <Routes>
-                    <Route path='*' element={<PageNotFound />} />
-                    <Route path='/' element={<Navigate to='/login' />} />
-                    <Route path='/login' element={<LoginPage />} />
-                    <Route path='/register' element={<RegisterPage />} />
-                    <Route path='/reset/password' element={<ResetPassword />} />
-                    <Route path='/reset/password/:uid/:token' element={<NewResetPassword />} />
-                    <Route element={<PrivateRoute />}>
-                      {/* Home */}
-                      <Route path='/home' element={<HomePage />} />
-                      {/* Account */}
-                      <Route path='/account/profile' element={<ProfilePage />} />
-                      <Route path='/account' element={<AccountPage />} />
-                      {/* Patients */}
-                      <Route path='/patients' element={<PatientsPage />} />
-                      <Route
-                        path='/patients/create'
-                        element={<PatientCreatePage />}
-                      />
-                      <Route
-                        path='/patients/edit/:id'
-                        element={<PatientEdit />}
-                      />
-                      <Route path='/patients/:id' element={<PatientDetail />} />
-                      {/* Prescriptions */}
-                      <Route
-                        path='/prescriptions'
-                        element={<PrescriptionsPage />}
-                      />
-                      <Route
-                        path='/prescriptions/:id'
-                        element={<PrescriptionEdit />}
-                      />
-                      <Route
-                        path='/prescriptions/create'
-                        element={<PrescriptionCreatePage />}
-                      />
-                    </Route>
-                  </Routes>
-                </Container>
-                <Footer />
-              </FlashMessageContextProvider>
-            </ProfileContextProvider>
-          </TokenContextProvider>
-        </OneSignalManager>
+        {/* <OneSignalManager> */}
+        <TokenContextProvider>
+          <ProfileContextProvider>
+            <FlashMessageContextProvider>
+              <Header />
+              <Container className='mb-5 pb-5'>
+                <FlashMessages />
+                <Routes>
+                  <Route path='*' element={<PageNotFound />} />
+                  <Route path='/' element={<Navigate to='/login' />} />
+                  <Route path='/login' element={<LoginPage />} />
+                  <Route path='/register' element={<RegisterPage />} />
+                  <Route path='/reset/password' element={<ResetPassword />} />
+                  <Route path='/reset/password/:uid/:token' element={<NewResetPassword />} />
+                  <Route element={<PrivateRoute />}>
+                    {/* Home */}
+                    <Route path='/home' element={<HomePage />} />
+                    {/* Account */}
+                    <Route path='/account/profile' element={<ProfilePage />} />
+                    <Route path='/account' element={<AccountPage />} />
+                    {/* Patients */}
+                    <Route path='/patients' element={<PatientsPage />} />
+                    <Route
+                      path='/patients/create'
+                      element={<PatientCreatePage />}
+                    />
+                    <Route
+                      path='/patients/edit/:id'
+                      element={<PatientEdit />}
+                    />
+                    <Route path='/patients/:id' element={<PatientDetail />} />
+                    {/* Prescriptions */}
+                    <Route
+                      path='/prescriptions'
+                      element={<PrescriptionsPage />}
+                    />
+                    <Route
+                      path='/prescriptions/:id'
+                      element={<PrescriptionEdit />}
+                    />
+                    <Route
+                      path='/prescriptions/create'
+                      element={<PrescriptionCreatePage />}
+                    />
+                  </Route>
+                </Routes>
+              </Container>
+              <Footer />
+            </FlashMessageContextProvider>
+          </ProfileContextProvider>
+        </TokenContextProvider>
+        {/* </OneSignalManager> */}
       </BrowserRouter>
     </div>
   )
