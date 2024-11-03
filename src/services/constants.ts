d750d6817582f2638cd0fb52e631fdd07d07fdf0
Andre Miras <andre.miras@gmail.com>
2024-11-03 01:03:10 +0100

    :alien: Migrate to versioned API, fixes #332

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -2,12 +2,18 @@ import assert from 'assert'
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8000'
 assert(typeof BACKEND_URL === 'string')
+const API_ROOT = `${BACKEND_URL}/api`
+const API_V1 = `${API_ROOT}/v1`
+const API_V2 = `${API_ROOT}/v2`
 
 const USER_DATE_FORMAT = 'dd/MM/yyyy'
 const BACKEND_DATE_FORMAT = 'yyyy-MM-dd'
 
 export {
   BACKEND_URL,
+  API_ROOT,
+  API_V1,
+  API_V2,
   USER_DATE_FORMAT,
   BACKEND_DATE_FORMAT
 }
