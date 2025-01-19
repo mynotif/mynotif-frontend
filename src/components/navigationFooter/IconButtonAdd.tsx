ccdf7834ab464c324f3236e819f4283fee32345d
Issa <issa.djehon@gmail.com>
2025-01-19 16:28:10 +0100

    :sparkles: Add access control to feature creation fixes #355
    - Implement subscription-based access control for IconButtonAdd
    - Add modal to prompt users to subscribe when trying to access restricted features
    - Modify IconButtonAdd to check subscription status before allowing action
    - Create modal dialog for subscription upsell

--- src/components/navigationFooter/IconButtonAdd.tsx
+++ src/components/navigationFooter/IconButtonAdd.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
 import { Button } from '../forms/inputGroups/Button'
+import ModalConfirmSend from '../modal/ModalConfirmSend'
+import { useNavigate } from 'react-router-dom'
+import { useSubscription } from '../../hook/subscription'
 
 interface IconButtonAddProps {
     onClick: () => void
@@ -7,11 +11,44 @@ interface IconButtonAddProps {
 }
 
 export const IconButtonAdd = ({ onClick, icon, location }: IconButtonAddProps) => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const navigate = useNavigate()
+    const { subscription } = useSubscription();
+    const isActive = subscription.active
     const isDisable = location.pathname === '/patients/create' ||
     location.pathname === '/prescriptions/create' ? true : false
 
+    const handleClick = () => {
+        if (isActive) {
+          onClick();
+        } else {
+          setIsModalVisible(true);
+        }
+    };
+
+    const closeModal = () => setIsModalVisible(false);
+
+    const goToSubscription = () => {
+        closeModal()
+        navigate('/subscription')
+    }
+
     return (
-    <div onClick={onClick} className="flex flex-col items-start absolute -top-16 -right-8 z-10">
+    <>
+    <div onClick={handleClick} className="flex flex-col items-start absolute -top-16 -right-8 z-10">
         <Button disabled={isDisable} icon={icon} iconTheme='accent' variant='icon' size='large' />
     </div>
+
+    {isModalVisible && (
+        <ModalConfirmSend
+            confirmText='Vous devez souscrire à un abonnement pour accéder à cette fonctionnalité'
+            handleClose={closeModal}
+            noText='Annuler'
+            show={isModalVisible}
+            onConfirm={goToSubscription}
+            yesText='Souscrire'
+            title='Abonnement'
+        />
+    )}
+    </>
 )}
