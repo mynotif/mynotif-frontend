89f2179b32da588e423534c9e3ed18259add8da6
Issa <issa.djehon@gmail.com>
2025-01-26 17:23:49 +0100

    update file config

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,23 @@
 module.exports = {
   webpack: {
-      configure: {
-        resolve: {
-          fallback: {
-            "assert": require.resolve("assert"),
-          },
-        },
-      },
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        "assert": require.resolve("assert")
+      };
+
+      webpackConfig.module.rules.push({
+        test: /\/node_modules\/lucide-react\/.*/,
+        use: 'babel-loader',
+      });
+
+      return webpackConfig;
     },
+  },
+  jest: {
+    configure: {
+      transformIgnorePatterns: [
+        'node_modules/(?!(lucide-react)/)'
+      ]
+    }
+  }
 }
