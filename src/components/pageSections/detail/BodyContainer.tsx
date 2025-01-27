2998c02e71b217cd31ca8fa2b947b8e4a829d245
Issa <issa.djehon@gmail.com>
2025-01-27 16:23:22 +0100

    :fire: Removes several unused components from the project

--- src/components/pageSections/detail/BodyContainer.tsx
+++
@@ -1,9 +0,0 @@
-interface BodyContainerProps {
-  children: React.ReactNode
-}
-
-export const BodyContainer = ({ children }: BodyContainerProps): JSX.Element => {
-  return (
-    <div className='pt-4 space-y-4'>{children}</div>
-  )
-}
